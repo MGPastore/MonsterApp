c77b5137a1a84a50c281551c2bd4d481801dee66
MGPastore <137975449+MGPastore@users.noreply.github.com>
2023-10-28 20:52:11 +0000

    Next_react_ionic

---
+++ front/src/App.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+
+function Home() {
+  return (
+    <div>
+      <h2>Home Page</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About Page</h2>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
